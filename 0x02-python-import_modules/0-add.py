a3bf0212e2fa5945a29a52a5d948d016e189afc0
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2023-12-05 01:15:55 -0800

    fix not run when importing by other programs

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == '__main__':
 
-a = 1
-b = 2
+    import add_0
 
-print(f'{a} + {b} = {add_0.add(a,b)}')
+    a = 1
+    b = 2
+
+    print(f'{a} + {b} = {add_0.add(a,b)}')
