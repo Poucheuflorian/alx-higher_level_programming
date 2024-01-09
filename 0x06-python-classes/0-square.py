ae6cc4d68da86b3449515136cf33d11bced7d446
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2024-01-08 18:25:06 -0800

    first commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains one class
+the name is Square
+"""
+class Square():
+    """
+    This is an empty class named Square.
+    it will be the first class of our journey in alx with python
+    """
+    pass
+
+if __name__ == "__main__":
+    print("hello Square")
