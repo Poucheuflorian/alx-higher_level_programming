56eb5abb23877526b73b632e6065b049fc5dee4e
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2024-01-16 18:12:50 -0800

    fix on line error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x = 0):
     """function that prints x elements of a list
     """
     for i in range(x):
         try:
-           print( my_list[i],end = "")
+           print( my_list[i], end = "")
         except IndexError:
+            print("")
             return i
+    print("")
     return x
 
 if __name__ == '__main__':
-    print(safe_print_list([1,2,3],5))
+    mylist = [1, 2, 3, 4]
+    print(safe_print_list(mylist, len(mylist)))
