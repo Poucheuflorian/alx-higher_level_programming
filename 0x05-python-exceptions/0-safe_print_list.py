f864be2e30a317b016a15017141a35078249a6a1
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2024-01-16 18:24:48 -0800

    fix pydecodetest : difference between space in C and python

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x = 0):
+def safe_print_list(my_list=[], x=0):
     """function that prints x elements of a list
     """
     for i in range(x):
         try:
-           print( my_list[i], end = "")
+            print(my_list[i], end="")
         except IndexError:
             print("")
             return i
     print("")
     return x
+
 
 if __name__ == '__main__':
     mylist = [1, 2, 3, 4]
