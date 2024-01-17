2a5961ff2d65e168f9f484ac67cb582e3c3e9135
poucheuflorian <poucheumbondaflorianwilfried@gmail.com>
2024-01-16 18:45:42 -0800

    Fix TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,6 @@ def safe_print_integer(value):
     """Write a function that prints an integer with "{:d}".format()"""
     try:
         print('{:d}'.format(value))
-    except ValueError:
+    except (ValueError,TypeError):
         return False
     return True
