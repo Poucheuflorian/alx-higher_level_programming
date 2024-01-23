36bb0e2e16c9b0bc9a2a0cf81d5e9d025059e79f
Gertrude <kapigertrude97@gmail.com>
2024-01-22 18:35:09 -0800

    First commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+class Rectangle():
+    """ Real Class Resctangle """
+    def __init__(self, width=0, height=0):
+        if(type(width) != int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError ("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width*self.__height
+
+    def perimeter(self):
+        return 2*(self.__width +self.__height)
+
+    @property
+    def width(self):
+        return self.__width
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self,width):
+        if(type(width) != int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
+    def height(self,height):
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError ("height must be >= 0")
+        self.__height = height
+
+    pass
